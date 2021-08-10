3c1fb13473932edf305ddb57475964b4353bfdb3
G Soros <gsoros@users.noreply.github.com>
2021-08-10 18:51:24 +0200

    null safety fixes +autoformat

--- example/lib/device_details/view/button_view.dart
+++ example/lib/device_details/view/button_view.dart
@@ -11,9 +11,7 @@ class ButtonView extends StatelessWidget {
     return Expanded(
       child: Padding(
         padding: const EdgeInsets.all(2.0),
-        child: RaisedButton(
-          color: Colors.blue,
-          textColor: Colors.white,
+        child: ElevatedButton(
           child: Text(_text),
           onPressed: action,
         ),
